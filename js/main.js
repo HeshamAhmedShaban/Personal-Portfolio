f0c8974bb8e2651bee827553f3fa0bf5adea1406
Hesham Ahmed <heshamshaaban0098@gmail.com>
2024-05-07 09:33:12 +0300

    Started In My Personal Portfolio And Finsh haeder and sideMenu

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+const divMenu= document.querySelector(".menu");
+
+const menuTrigger = document.querySelector(".menu-trigger");
+
+menuTrigger.addEventListener("click", ()=>{
+    setTimeout(function(){
+        if(!divMenu.classList.contains("show")){
+            divMenu.classList.add("show");
+            document.body.classList.add("menu-visible");
+        }
+    },0)
+})
+
+
+// auto close menu by clicking outside
+
+document.addEventListener("click", (e)=>{
+    if(!e.target.closest(".menu") && divMenu.classList.contains("show")){
+        divMenu.classList.remove("show");
+        document.body.classList.remove("menu-visible");
+    }
+})
